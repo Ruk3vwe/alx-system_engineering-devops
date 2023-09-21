34ba933b0d9df64c84c4947d9474673bf60c476e
Ruk3vwe <anitod@live.co.uk>
2023-09-21 13:34:02 -0700

    API ADV

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,49 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 """
-Created on Thu Sep 21 03:39 2023
+Created on Thu Sep 21 03:29 2023
+
 @author: Anita Ododo
 """
+from json import loads
+from requests import get
 
-import requests
 
-def recurse(subreddit, hot_list=[], after=None):
-    """
-    Recursive function that queries the Reddit API and returns a list
+def recurse(subreddit, hot_list=[]):
+    """recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit. If no
     results are found for the given subreddit, the function should return None.
     """
-    if not subreddit:
-        return None
-
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    params = {'after': after}
-    
-    response = requests.get(url, headers=headers, params=params)
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
 
-    if response.status_code != 200:
-        print(f"Failed to fetch data for subreddit: {subreddit}")
-        return None
-
-    data = response.json()
-    children = data.get('data', {}).get('children', [])
-
-    for title in children:
-        hot_list.append(title.get('data', {}).get('title'))
-
-    after = data.get('data', {}).get('after')
-
-    if after:
-        # Recursive call to fetch the next page
-        return recurse(subreddit, hot_list, after)
-    else:
+    try:
+        children = reddits.get('data').get('children')
+        for title in children:
+            hot_list.append(title.get('data').get('title'))
         return hot_list
-
-# Example usage:
-# hot_articles = recurse('programming')
-# print(hot_articles)
+    except:
+        print(None)
+        return 0
