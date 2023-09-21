3358dc29aedc7ec78350eed6256ef8e24ccc118e
Ruk3vwe <anitod@live.co.uk>
2023-09-21 12:55:57 -0700

    API ADV

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 21 03:39 2023
 
-@author: Anita Ododo
-"""
-from json import loads
 from requests import get
 
-
-def recurse(subreddit, hot_list=[]):
-    """recursive function that queries the Reddit API and returns a list
+def recurse(subreddit, hot_list=[], after=None):
+    """Recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit. If no
     results are found for the given subreddit, the function should return None.
     """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {
         'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) '
+        'Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    response = get(url, headers=headers, allow_redirects=False)
+    params = {'after': after} if after else {}
+    
+    response = get(url, headers=headers, params=params, allow_redirects=False)
+    
+    if response.status_code != 200:
+        print(f"Failed to retrieve data for subreddit '{subreddit}'")
+        return None
+    
     reddits = response.json()
+    children = reddits.get('data', {}).get('children', [])
 
-    try:
-        children = reddits.get('data').get('children')
-        for title in children:
-            hot_list.append(title.get('data').get('title'))
+    if not children:
         return hot_list
-    except:
-        print(None)
-        return 0
+
+    for title in children:
+        hot_list.append(title.get('data', {}).get('title'))
+
+    after = reddits.get('data', {}).get('after')
+
+    if after:
+        return recurse(subreddit, hot_list, after)
+    else:
+        return hot_list
+
+# Example usage:
+# hot_titles = recurse('python')
+# print(hot_titles)
