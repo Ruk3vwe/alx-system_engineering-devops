5b58cc0a29fae24ebce7049d9204d96b0088ec05
Ruk3vwe <anitod@live.co.uk>
2023-09-21 13:57:09 -0700

    API ADV

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,11 +13,7 @@ def recurse(subreddit, hot_list=[]):
     """recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit. If no
     results are found for the given subreddit, the function should return None.
-    """
-
-    if not subreddit:
-        return None
-    
+    """    
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {
         'User-Agent':
